b7c9ba45052976d228617a05d277131df835fd3d
saket kumar <kumarsaket601@gmail.com>
2021-08-11 15:57:52 +0530

    update in js

--- app.js
+++ app.js
@@ -4,28 +4,30 @@ var currentPrice = document.querySelector("#current-price");
 var checkButton = document.querySelector("#check-button");
 var display = document.querySelector(".output");
 
-function percentageLoss(lossesBook,invested){
+function percentageLoss(lossesBook, invested) {
 
-    var loss = (lossesBook / invested)*100;
+    var loss = (lossesBook / invested) * 100;
 
     return loss;
 
 }
-function lossInPrice(invested,currentValue){
+
+function lossInPrice(invested, currentValue) {
 
     var currentLoss = invested - currentValue;
-    
+
     return currentLoss;
 }
+
 function calculatePercentageGain(profitBook, invested) {
 
-    var percentGained = (profitBook / invested)*100;
+    var percentGained = (profitBook / invested) * 100;
 
     return percentGained;
 
 }
 
-function changeInPrice(invested,currentValue) {
+function changeInPrice(invested, currentValue) {
 
     var difference = currentValue - invested;
 
@@ -39,26 +41,26 @@ function stockPriceHandler() {
     var invested = Number(purchasePrice.value) * Number(stockQuantity.value);
     var currentValue = Number(currentPrice.value) * Number(stockQuantity.value);
 
-    if (currentValue >= invested) {
+    if ( Number(currentPrice.value) >= Number(purchasePrice.value)) {
 
-        var profitBook = changeInPrice(invested,currentValue);
+        var profitBook = changeInPrice(invested, currentValue);
 
-        var percentGained = calculatePercentageGain(profitBook,invested);
+        var percentGained = calculatePercentageGain(profitBook, invested);
 
-        
+        display.innerText = "Your profit is " + profitBook + " and Percent Gained " + percentGained + "%.";
+
+    } else {
+
+        var lossesBook = lossInPrice(invested, currentValue);
+        var losses = percentageLoss(lossesBook, invested);
+
+         display.innerText = "You lossed  " + lossesBook + " and Percent Loss " + losses + "%.";
+
     }
 
-    else{
+    
 
-        var lossesBook = lossInPrice(invested,currentValue);
-        var losses = percentageLoss(lossesBook,invested);
-
-         
-
-    }
-       display.innerText = "Your profit is " + profitBook + " and Percent Gained " + percentGained + "%.";
-
-       display.innerText = "You lossed  " + lossesBook + " and Percent Loss " + losses + "%.";  
+   
 
 
 
